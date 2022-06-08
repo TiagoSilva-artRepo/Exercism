766ac5f050a4e88b3cf6917f4a10d5932b544e7f
Tiago Silva <tiago.silva@contisystems.pt>
2022-06-08 14:45:51 +0100

    add gigasecond exercise solution

---
+++ javascript/gigasecond/gigasecond.js
@@ -0,0 +1,6 @@
+//
+// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+const GIGASECOND_IN_MS = Math.pow(10, 12);
+export const gigasecond = (moment) => new Date(moment.getTime() + GIGASECOND_IN_MS);
